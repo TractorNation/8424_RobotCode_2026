b31abac62fd447c2ec7e78cc69afd81e98be563f
Flatdog404 <156031436+Flatdog404@users.noreply.github.com>
2026-02-10 16:09:44 -0500

    Added foundations for shooter subsystem
    Added folder, constants file, and subsystem file

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+}
